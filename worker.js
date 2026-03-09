82d594fa8b20590aaa13984269206e9cf9e43d3c
computerfriends <jack@computerfriends.me>
2026-03-09 12:15:06 -0400

    move getWeek outside of isBWeek()

--- worker.js
+++ worker.js
@@ -188,6 +188,22 @@
     }
   });
 
+
+
+
+  // Returns the ISO week of the date.
+  Date.prototype.getWeek = function() {
+  var date = new Date(this.getTime());
+  date.setHours(0, 0, 0, 0);
+  // Thursday in current week decides the year.
+  date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
+  // January 4 is always in week 1.
+  var week1 = new Date(date.getFullYear(), 0, 4);
+  // Adjust to Thursday in week 1 and count number of weeks from date to week1.
+  return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000 - 3 + (week1.getDay() + 6) % 7) / 7);
+  }
+
+
   // index.js
   function getPatchTuesday() {
 
@@ -225,19 +241,6 @@
   }
 
   function isBWeek() {
-
-    // Returns the ISO week of the date.
-  Date.prototype.getWeek = function() {
-    var date = new Date(this.getTime());
-    date.setHours(0, 0, 0, 0);
-    // Thursday in current week decides the year.
-    date.setDate(date.getDate() + 3 - (date.getDay() + 6) % 7);
-    // January 4 is always in week 1.
-    var week1 = new Date(date.getFullYear(), 0, 4);
-    // Adjust to Thursday in week 1 and count number of weeks from date to week1.
-    return 1 + Math.round(((date.getTime() - week1.getTime()) / 86400000
-                          - 3 + (week1.getDay() + 6) % 7) / 7);
-  }
 
     const date = new Date()
 
